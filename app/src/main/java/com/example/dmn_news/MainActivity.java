816f9f34fa40d32597b321636b40cfe01a55a158
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-02-14 22:00:41 +0100

    initial commit

---
+++ app/src/main/java/com/example/dmn_news/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dmn_news;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this)
+                .load("https:/picsum.photos/seed/200/200")
+                .into(imageView);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> Toast.makeText(this, "Volley Works!", Toast.LENGTH_SHORT).show(),
+                error -> Toast.makeText(this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show());
+        queue.add(stringRequest);
+
+    }
+}
