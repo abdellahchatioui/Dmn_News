b4f4b970a444d0bc004d5c359dd67efdc1a47719
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-02-16 14:26:13 +0100

    Change splash style.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("org.projectlombok:lombok:1.18.24")
+    
 }
